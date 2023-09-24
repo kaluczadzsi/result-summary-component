7ddd6178250ea953ea72a668dd2e1c17670f2fcd
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-09-24 20:53:04 +0200

    Results card styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <main>
+      <div className="card  font-hanken font-medium">
+        <figure className="rounded-bl-[30px] rounded-br-[30px] results bg-gradient-to-b from-light-slate-blue to-light-royal-blue flex flex-col  py-6 justify-center items-center">
+          <header className="text-light-lavender mb-4">Your Result</header>
+          <div className=" w-[130px] h-[130px] m-auto flex flex-col justify-center items-center border-1 rounded-full py- bg-gradient-to-b from-violet-blue to-persian-blue">
+            <h1 className="text-white text-[3rem] font-bold -mb-2">76</h1>
+            <span className="text-light-lavender text-sm opacity-[0.4]">
+              of 100
+            </span>
+          </div>
+          <p className="mt-4 mb-2 text-white text-lg">Great</p>
+          <p className="mb-2 text-[14px] w-[60%] m-auto text-light-lavender opacity-[0.8]">
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </figure>
+        <figure className="summary">
+          <header className="text-light-slate-blue">Summary</header>
+          <ul>
+            <li>Reaction 80 / 100</li>
+            <li>Memory 92 / 100</li>
+            <li>Verbal 61 / 100</li>
+            <li>Visual 72 / 100</li>
+          </ul>
+          <button>Continue</button>
+        </figure>
+      </div>
+    </main>
+  );
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        hanken: ['Hanken Grotesk', 'sans-serif'],
+      },
+      colors: {
+        // Gradients
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        // circle
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsla(241, 72%, 46%, 0)',
+        // Neutral
+        'pale-blue': 'hsl(221, 100%, 96%)',
+        'light-lavender': 'hsl(241, 100%, 89%)',
+        'Dark-gray-blue': 'hsl(224, 30%, 27%)',
+      },
+    },
   },
   plugins: [],
 };
