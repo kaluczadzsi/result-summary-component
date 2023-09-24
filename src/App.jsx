1ae987efb2258c4d6a14ba67cbab56fe9f24db8f
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-09-24 21:37:32 +0200

    Mobile styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <main>
-      <div className="card  font-hanken font-medium">
-        <figure className="rounded-bl-[30px] rounded-br-[30px] results bg-gradient-to-b from-light-slate-blue to-light-royal-blue flex flex-col  py-6 justify-center items-center">
+      <div className="card sm:h-screen sm:justify-center sm:items-center sm:max-w-[1440px] sm:flex font-hanken font-medium">
+        <figure className="rounded-bl-[30px] rounded-br-[30px] results bg-gradient-to-b from-light-slate-blue to-light-royal-blue flex flex-col py-6 justify-center items-center">
           <header className="text-light-lavender mb-4">Your Result</header>
           <div className=" w-[130px] h-[130px] m-auto flex flex-col justify-center items-center border-1 rounded-full py- bg-gradient-to-b from-violet-blue to-persian-blue">
             <h1 className="text-white text-[3rem] font-bold -mb-2">76</h1>
@@ -11,19 +11,55 @@ const App = () => {
             </span>
           </div>
           <p className="mt-4 mb-2 text-white text-lg">Great</p>
-          <p className="mb-2 text-[14px] w-[60%] m-auto text-light-lavender opacity-[0.8]">
+          <p className="text-center mb-2 text-[14px] w-[60%] m-auto text-light-lavender opacity-[0.8]">
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </figure>
-        <figure className="summary">
-          <header className="text-light-slate-blue">Summary</header>
-          <ul>
-            <li>Reaction 80 / 100</li>
-            <li>Memory 92 / 100</li>
-            <li>Verbal 61 / 100</li>
-            <li>Visual 72 / 100</li>
+        <figure className="px-6 summary py-6 flex flex-col">
+          <header className="self-left mb-6 text-dark-gray-blue font-extrabold">
+            Summary
+          </header>
+          <ul className="w-full flex flex-col gap-4 mb-8">
+            <li className="bg-light-red flex items-center justify-between p-4 rounded-lg">
+              <div className="flex gap-2">
+                <img src="src\assets\icon-reaction.svg" />
+                <span className="text-[#ff5757]">Reaction</span>
+              </div>
+              <div>
+                80 <span className="text-gray-400">/ 100</span>
+              </div>
+            </li>
+            <li className="bg-orangey-yellow flex items-center justify-between p-4 rounded-lg">
+              <div className="flex gap-2">
+                <img src="src\assets\icon-memory.svg" />
+                <span className="text-[#ffb01f]">Memory</span>
+              </div>
+              <div>
+                92 <span className="text-gray-400">/ 100</span>
+              </div>
+            </li>
+            <li className="bg-green-teal flex items-center justify-between p-4 rounded-lg">
+              <div className="flex gap-2">
+                <img src="src\assets\icon-verbal.svg" />
+                <span className="text-[#00bd91]">Verbal</span>
+              </div>
+              <div>
+                61 <span className="text-gray-400">/ 100</span>
+              </div>
+            </li>
+            <li className="bg-cobalt-blue flex items-center justify-between p-4 rounded-lg">
+              <div className="flex gap-2">
+                <img src="src\assets\icon-visual.svg" />
+                <span className="text-[#1125d4]">Visual</span>
+              </div>
+              <div>
+                72 <span className="text-gray-400">/ 100</span>
+              </div>
+            </li>
           </ul>
-          <button>Continue</button>
+          <button className="py-4 inline-block text-white border-2 rounded-full bg-dark-gray-blue">
+            Continue
+          </button>
         </figure>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ export default {
         hanken: ['Hanken Grotesk', 'sans-serif'],
       },
       colors: {
+        // Primary
+        'light-red': 'hsla(0, 100%, 67%, 0.1)',
+        'orangey-yellow': 'hsla(39, 100%, 56%, 0.1)',
+        'green-teal': 'hsla(166, 100%, 37%, 0.1)',
+        'cobalt-blue': 'hsla(234, 85%, 45%, 0.1)',
         // Gradients
         'light-slate-blue': 'hsl(252, 100%, 67%)',
         'light-royal-blue': 'hsl(241, 81%, 54%)',
@@ -16,7 +21,7 @@ export default {
         // Neutral
         'pale-blue': 'hsl(221, 100%, 96%)',
         'light-lavender': 'hsl(241, 100%, 89%)',
-        'Dark-gray-blue': 'hsl(224, 30%, 27%)',
+        'dark-gray-blue': 'hsl(224, 30%, 27%)',
       },
     },
   },
