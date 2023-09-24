ffdaa5a91a5233474d4bc02d25c7d4b14315f1dc
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-09-24 22:09:44 +0200

    All styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 const App = () => {
   return (
     <main>
-      <div className="card sm:h-screen sm:justify-center sm:items-center sm:max-w-[1440px] sm:flex font-hanken font-medium">
-        <figure className="rounded-bl-[30px] rounded-br-[30px] results bg-gradient-to-b from-light-slate-blue to-light-royal-blue flex flex-col py-6 justify-center items-center">
-          <header className="text-light-lavender mb-4">Your Result</header>
-          <div className=" w-[130px] h-[130px] m-auto flex flex-col justify-center items-center border-1 rounded-full py- bg-gradient-to-b from-violet-blue to-persian-blue">
-            <h1 className="text-white text-[3rem] font-bold -mb-2">76</h1>
+      <div className="card sm:mx-6 bg-white shadow-2xl rounded-[25px] sm:justify-center sm:items-center sm:max-w-[1440px] sm:flex font-hanken font-medium">
+        <figure className="sm:py-8 self-stretch sm:rounded-[25px] sm:  sm:flex-1 rounded-bl-[30px] rounded-br-[30px] results bg-gradient-to-b from-light-slate-blue to-light-royal-blue flex flex-col py-6 justify-center items-center">
+          <header className="text-light-lavender mb-4 sm:mb-0">
+            Your Result
+          </header>
+          <div className="sm:mb-8 sm:mt-8 sm:w-[180px] sm:h-[180px]  w-[130px] h-[130px] m-auto flex flex-col justify-center items-center border-1 rounded-full py- bg-gradient-to-b from-violet-blue to-persian-blue">
+            <h1 className="sm:text-[4rem] text-white text-[3rem] font-bold -mb-2">
+              76
+            </h1>
             <span className="text-light-lavender text-sm opacity-[0.4]">
               of 100
             </span>
           </div>
-          <p className="mt-4 mb-2 text-white text-lg">Great</p>
-          <p className="text-center mb-2 text-[14px] w-[60%] m-auto text-light-lavender opacity-[0.8]">
+          <p className="mt-4 mb-2 text-white text-lg sm:mb-0 sm:text-2xl">
+            Great
+          </p>
+          <p className=" text-center mb-2  text-[14px] w-[60%] m-auto text-light-lavender opacity-[0.8]">
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </figure>
-        <figure className="px-6 summary py-6 flex flex-col">
+        <figure className="sm:py-8 sm: flex-1 px-6 summary py-6 flex flex-col">
           <header className="self-left mb-6 text-dark-gray-blue font-extrabold">
             Summary
           </header>
