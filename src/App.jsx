ad05ad31f1b212c60751b6f2fee141ca7f88baa4
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-09-24 22:24:12 +0200

    Project completed

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
               </div>
             </li>
           </ul>
-          <button className="py-4 inline-block text-white border-2 rounded-full bg-dark-gray-blue">
+          <button className="transition: duration-300 ease-in-out hover:bg-gradient-to-b from-light-slate-blue to-light-royal-blue py-4 inline-block text-white border-2 rounded-full bg-dark-gray-blue">
             Continue
           </button>
         </figure>
